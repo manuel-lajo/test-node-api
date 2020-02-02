12dda0e8b3ba5e60e9b4e612a1fc23dfa21fb935
Manuel Lajo <manuel.lajo@autodesk.com>
2020-02-02 18:06:02 -0500

    CORS loig added to allow cross oring requests.

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app = express();
 
 const courseRoutes = require('./routes/course');
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  next();
+})
+
 app.use('/course', courseRoutes);
 
 app.listen(3000);
